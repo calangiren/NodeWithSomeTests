20fc481138dc01f8593764e92087d114dd470fbc
Irene Calangian <ireneann_94@yahoo.com.ph>
2017-09-21 00:49:03 +1200

    Changes

---
+++ app.js
@@ -0,0 +1,15 @@
+// module.exports = function(){
+// 	return'hello';
+// }
+
+module.exports = {
+	sayHello: function(){
+		return 'hello';
+		// return 1;
+	},
+	
+	addNumbers: function(value1, value2 ){
+		return value1+value2;
+	}
+	
+}
---
+++ test/appTest.js
@@ -0,0 +1,60 @@
+const assert = require('chai').assert; 
+// const sayHello = require('../app').sayHello;
+// const addNumbers = require('../app').addNumbers;
+
+const app = require('../server');
+
+// Results
+sayHelloResult = server.sayHello();
+addNumbersResult = server.addNumbers(5,5);
+portNumberResult = server.addNumbers(5,5);
+
+describe('App', function(){
+	"use strict";
+	
+	describe('sayHello()', function(){
+		it('sayHello should return hello', function(){
+			// let result = app.sayHello();
+			assert.equal(sayHelloResult,'hello');
+			// assert.equal(sayHello(),'hello');
+		});
+	
+		it('sayHello should return type String', function(){
+			// let result = app.sayHello();
+			assert.typeOf(sayHelloResult, 'string');
+
+		});
+		
+	});
+	
+	describe('addNumbers()', function(){
+		it('addNumbers should be above 5', function(){
+			// let result = app.addNumbers(5,5);
+			assert.isAbove(addNumbersResult,5);
+		});
+	
+		it('addNumbers should return type Integer', function(){
+			// let result = app.addNumbers(5,5);
+			assert.typeOf(addNumbersResult, 'number');
+
+		});	
+		
+	});
+	
+	describe('portNumber()', function(){
+		it('portNumber should be above 8080', function(){
+			// let result = app.addNumbers(5,5);
+			assert.equal(portNumberResult, 8080);
+		});
+	
+		// it('addNumbers should return type Integer', function(){
+		// 	// let result = app.addNumbers(5,5);
+		// 	assert.typeOf(addNumbersResult, 'number');
+		//
+		// });
+		
+	});
+	
+	
+
+}); 
